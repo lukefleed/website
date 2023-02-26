3fcee0e5716210db8fd22f351ae9c0afcdbfb6b2
Luca Lombardo <l.lombardo@protonmail.com>
2023-02-26 15:43:19 +0100

    first commit

---
+++ src/config.ts
@@ -0,0 +1,141 @@
+import type { SocialObjects } from "./types";
+
+export const SITE = {
+  website: "https://lukefleed.xyz",
+  author: "Luca Lombardo",
+  desc: "My personal website",
+  title: "Luca Lombardo",
+  ogImage: "Duke_Fleed.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 5,
+};
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/lukefleed",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Facebook",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: false,
+  },
+  {
+    name: "Instagram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Instagram`,
+    active: false,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/l-lombardo/",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Mail",
+    href: "mailto:l.lombardo@pm.me",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: false,
+  },
+  {
+    name: "Twitch",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Twitch`,
+    active: false,
+  },
+  {
+    name: "YouTube",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on YouTube`,
+    active: false,
+  },
+  {
+    name: "WhatsApp",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on WhatsApp`,
+    active: false,
+  },
+  {
+    name: "Snapchat",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Snapchat`,
+    active: false,
+  },
+  {
+    name: "Pinterest",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Pinterest`,
+    active: false,
+  },
+  {
+    name: "TikTok",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on TikTok`,
+    active: false,
+  },
+  {
+    name: "CodePen",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on CodePen`,
+    active: false,
+  },
+  {
+    name: "Discord",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Discord`,
+    active: false,
+  },
+  {
+    name: "GitLab",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on GitLab`,
+    active: false,
+  },
+  {
+    name: "Reddit",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Reddit`,
+    active: false,
+  },
+  {
+    name: "Skype",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Skype`,
+    active: false,
+  },
+  {
+    name: "Steam",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Steam`,
+    active: false,
+  },
+  {
+    name: "Telegram",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Telegram`,
+    active: false,
+  },
+  {
+    name: "Mastodon",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Mastodon`,
+    active: false,
+  },
+];
---
+++ src/contents/leetcode.md
@@ -0,0 +1,32 @@
+---
+author: Luca Lombardo
+datetime: 2022-12-01
+title: leetcode solutions
+slug: leetcode
+featured: false
+draft: false
+tags:
+  - Competitive Programming
+  - Algorithms and Data Structures
+  - Python
+ogImage: ""
+description: Solutions to some of the problems I have solved on Leetcode
+---
+
+Github repository: [leetcode](https://github.com/lukefleed/leetcode)
+
+In this repository you can find my solutions to some of the problems I have solved on Leetcode.
+
+### Resources that helped me
+
+- [Neetcode](https://neetcode.io/practice)
+
+Three _must read_ books:
+
+- [The Algorithm Design Manual](https://books.google.it/books/about/The_Algorithm_Design_Manual_Text.html?id=TrXd-gxPhVYC)
+- [Competitive Programmer's Handbook](https://github.com/pllk/cphb)
+- [Cracking the Coding Interview](https://books.google.it/books/about/Cracking_the_Coding_Interview.html?id=jD8iswEACAAJ)
+
+An incomplete summary of the most important algorithms and data structures to remember before a coding interview:
+
+- [Algorithms and Data Structures](howtoasd)
